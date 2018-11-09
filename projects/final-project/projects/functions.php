aba2f3cf5d631a7bd93bc9671fbf51968bb9265f
olimejj <olimejj@gmail.com>
2018-11-08 17:56:29 -1000

    Responsive page done but still needs media tags css

---
+++ projects/final-project/projects/functions.php
@@ -0,0 +1,50 @@
+<?php 
+
+function make_header($current_link, $crumbs){?>
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<link rel="stylesheet" type="text/css" href="debt_style.css">
+			<title>Debt Journey | <?php echo $current_link ?></title>
+			<meta charset="uft-8">
+		</head>
+		<body>
+			<?php $crum_num=0;
+			foreach($crumbs as $key => $value){
+				if($crum_num >=1){echo '<a>&#8592;</a>';}
+				crumbs_generator($key, $value);
+				$crum_num++;
+			}		
+			nav_bar($current_link);?>
+			<div class='wrapper'>
+<?php 
+}
+function make_footer(){
+	?>
+	</div>
+		<footer>
+			<p>&copy; olimejj-creations</p>
+			<p id="email">olimejj@gmail.com</p>
+		</footer>
+	</body>
+	</html>
+<?php
+}
+function nav_bar($current_link){
+	echo '<nav>';
+		nav_link("HOME", "index.php", $current_link);
+		nav_link("CHART", "table.php", $current_link);
+		nav_link("FORM", "form.php", $current_link);
+	echo '</nav>';
+}
+function nav_link($name, $url, $current_link) {
+  if ($name == $current_link) { ?>
+      <a href="<?php print $url ?>" class="on"><?php print $name ?></a>
+  <?php }else{?>
+  <a href="<?php print $url ?>"><?php print $name ?></a><?php
+  }
+}
+function crumbs_generator($name, $url){
+	echo "<a href=\"$url\">$name</a>";
+}
+?>
---
+++ projects/final-project/projects/index.php
@@ -0,0 +1,24 @@
+<?php
+	require_once("functions.php");
+	make_header("HOME",array("HOME"=>"index.php"));			
+?>
+			<figure id="debt-tiper">
+				<img src="debt-boulder.png" alt="picture of person pushing boulder representing debt">
+				<figcaption>Do You Feel Like This Guy?</figcaption>
+			</figure>
+			<h2>What Its Like To Look At Your Finances</h2>
+			<audio controls="controls">
+				<source src="gloom.mp3" type="audio/mpeg">
+				<source src="gloom.ogg" type="audio/ogg">
+				<a href="gloom.mp3">Download Audio File</a>
+			</audio>
+			<h2>Video</h2>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/7w7ogwv8huc" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<h2>What Its Like To Pay Off Your Debt!</h2>
+			<audio controls="controls">
+				<source src="happy.mp3" type="audio/mpeg">
+				<source src="happy.ogg" type="audio/ogg">
+				<a href="happy.mp3">Download Audio File</a>
+			</audio>
+			<a href="#" class="nav-link">Get Out Of Debt Calculator</a>
+<?php make_footer();?>
