7bff86d30c1a461eb347c2c781f82cf6599a0bfa
olimejj <olimejj@gmail.com>
2018-12-04 09:56:00 -1000

    small updates

--- projects/final-project/books.php
+++ projects/final-project/books.php
@@ -10,14 +10,17 @@
 						<img src="assets/makeover.jpg" alt="Dave Ramseys book the total money makeover" id="makeover-pic">
 						<figcaption>A classic book by a classic author</figcaption>
 					</figure>
+					<a href="https://www.amazon.com/Total-Money-Makeover-Classic-Financial-ebook/dp/B00DNBE8P6/ref=sr_1_1?ie=UTF8&qid=1543948279&sr=8-1&keywords=the%20total%20money%20makeover%20by%20dave%20ramsey&fbclid=IwAR1v7sRSSjf0v0V67oC6PuHvCGfRrT99xIqpLUMMxf8i0ulwI-AoBTa5EiI">Total Money Makeover Amazon</a>
 					<figure id="makeover">
 						<img src="assets/babylon.jpg" alt="Dave Ramseys book the total money makeover" id="makeover-pic">
 						<figcaption>A classic book by a classic author</figcaption>
 					</figure>
+					<a href="https://www.amazon.com/Richest-Man-Babylon-George-Clason-ebook/dp/B07H7HN6DN/ref=sr_1_2?ie=UTF8&qid=1543947956&sr=8-2&keywords=the%20richest%20man%20in%20babylon&fbclid=IwAR1WmqE4etQ3obWUUTe9_6q14PXKU21RZc4dXMl34YsHM_9FWGnz_HuLT3M">The Richest Man In Babylon Amazon</a>
 					<figure id="makeover">
 						<img src="assets/zero-debt.jpg" alt="Dave Ramseys book the total money makeover" id="makeover-pic">
 						<figcaption>A classic book by a classic author</figcaption>
 					</figure>
+					<a href="https://www.amazon.com/Zero-Debt-Ultimate-Financial-Freedom-ebook/dp/B071S9RWLV/ref=sr_1_2?ie=UTF8&qid=1543948384&sr=8-2&keywords=zero%20debt%20the%20ultimate%20guide%20to%20financial%20freedom&fbclid=IwAR0r_WK0IeKfPDMjJ4YJU34wG0HKan0PhBKt8DZXyygXCsCSaU7yUEvbezM">Zero Debt Amazon</a>
 						<p></p><br class="clear">
 				</div></div>
 </div>
