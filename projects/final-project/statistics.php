c0232f9ec0a25911d8160c7421e2d7250fd1b1b4
olimejj <olimejj@gmail.com>
2018-12-04 09:37:08 -1000

    updated footer and added video to stats

--- projects/final-project/statistics.php
+++ projects/final-project/statistics.php
@@ -8,8 +8,12 @@
 		    <li>73 percent of consumers had outstanding debt when they were reported as dead</li>
 		    <li> Of the 73 percent about 68 percent had credit card balances</li>
 		</ul>
+		<p>Another great problem we are facing today is a masive rise in student loans. The average graduating student now carries over $30,000 in loans. This video by cbsnews illistrates the situation well:</p>
+		<iframe src="https://www.cbsnews.com/video/more-than-75-million-millennials-struggling-with-student-debt/" id="cbsNewsVideo" frameborder="0" width="620" height="349"></iframe>
 		<p>So what is going on in America? These statistics can be found on <a href ="https://www.cbsnews.com/news/americans-are-dying-with-an-average-of-62k-of-debt">cbsnews.com</a>
 		The article goes on to say <q>You’re probably going to die with some debt to your name. Most people do</q> many people belive today that having debt is normal and as long as you manage it there is no problem however we are going to see from some great financial advisors how ridicules that ideology realy is.</p>
+
+	
 
 	</div>
 <?php make_footer();?>
