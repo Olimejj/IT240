d1f623a3164650eeb8ef33af2651d4cf97509928
olimejj <olimejj@gmail.com>
2018-12-04 08:15:57 -1000

    added content

--- projects/final-project/statistics.php
+++ projects/final-project/statistics.php
@@ -2,4 +2,14 @@
 	require_once("functions.php");
 	make_header("ABOUT DEBT",array("HOME"=>"index.php","ABOUT DEBT"=>"statistics.php"));			
 ?>
+	<div id="statistics">
+		<p>Some scary statistics on debt in America:</p>
+		<ul><li>Americans are dying with an average of $62,000 of debt</li>
+		    <li>73 percent of consumers had outstanding debt when they were reported as dead</li>
+		    <li> Of the 73 percent about 68 percent had credit card balances</li>
+		</ul>
+		<p>So what is going on in America? These statistics can be found on <a href ="https://www.cbsnews.com/news/americans-are-dying-with-an-average-of-62k-of-debt">cbsnews.com</a>
+		The article goes on to say <q>You’re probably going to die with some debt to your name. Most people do</q> many people belive today that having debt is normal and as long as you manage it there is no problem however we are going to see from some great financial advisors how ridicules that ideology realy is.</p>
+
+	</div>
 <?php make_footer();?>
