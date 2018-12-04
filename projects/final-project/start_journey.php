e8ab5b0105d9093803d5e8032946e333a0fdb448
olimejj <olimejj@gmail.com>
2018-12-04 11:52:43 -1000

    added mango tree and info on stats

--- projects/final-project/start_journey.php
+++ projects/final-project/start_journey.php
@@ -2,7 +2,11 @@
 	require_once("functions.php");
 	make_header("START JOURNEY",array("HOME"=>"index.php","START JOURNEY"=>"start_journey.php"));			
 ?>
+<figure id="mango">
+                                                <img src="assets/mango.jpg" alt="A buitiful Mango Tree" id="mango-pic">
+                                                <figcaption>Majestic Mango painting source Boston Public Lirary</figcaption>
+                                        </figure>
 <blockquote> the best time to plant a tree was 20 years ago. The second best time is now. –Chinese Proverb</blockquote>
-<p> The most important thing to do is to START! Once started stick with it and keep going. Everybody will work at a different pace and are in different phases of life. One thing is for sure you will ,as the Chinese proverb quoted above states, be grateful you did in 20 years.
+<p> The most important thing to do is to START! Once started stick with it and keep going. Everybody will work at a different pace and are in different phases of life. One thing is for sure you will ,as the Chinese proverb quoted above states, be grateful you did in 20 years.</p>
 
 <?php make_footer();?>
