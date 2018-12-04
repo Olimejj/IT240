15cc3ffeab02ebc27ebee6b58273d1d6170ed353
olimejj <olimejj@gmail.com>
2018-12-04 13:41:30 -1000

    validation

--- projects/final-project/books.php
+++ projects/final-project/books.php
@@ -9,17 +9,19 @@
 					<figure class="makeover">
 						<img src="assets/makeover.jpg" alt="Dave Ramseys book the total money makeover" id="makeover-pic">
 						<figcaption>A classic book by a classic author</figcaption>
+
+					</figure><div class="makeover-right">
 					<p>The Total Money Makeover is a no nonsense book on getting out of debt<p>
-					<p>Ramsy says about it: <q>if you’re tired of the lies and sick of the false promises, take a look at this—it’s the simplest, most straightforward game plan for completely making over your money habits. And it’s based on results, not pie-in-the-sky fantasies</q>
+					<p>Ramsy says about it: <q>if you’re tired of the lies and sick of the false promises,<br> take a look at this—it’s the simplest, most straightforward game plan for completely making over your money habits.<br> And it’s based on results, not pie-in-the-sky fantasies</q>
 					<a href="https://www.amazon.com/Total-Money-Makeover-Classic-Financial-ebook/dp/B00DNBE8P6/ref=sr_1_1?ie=UTF8&qid=1543948279&sr=8-1&keywords=the%20total%20money%20makeover%20by%20dave%20ramsey&fbclid=IwAR1v7sRSSjf0v0V67oC6PuHvCGfRrT99xIqpLUMMxf8i0ulwI-AoBTa5EiI">Total Money Makeover Amazon</a>
 					<figure class="makeover-right">
 						<img src="assets/babylon.jpg" alt="Dave Ramseys book the total money makeover" id="babylon-pic">
 						<figcaption>A classic book by a classic author</figcaption>
 					</figure>
-					<p>Acclaimed as a modern-day classic, this celebrated bestseller offers an understanding of — and a solution to — your personal financial problems that will guide you through a lifetime. This is the book that holds the secrets to keeping your money — and making more. </p>
+					<p>Acclaimed as a modern-day classic, this celebrated bestseller offers an<br> understanding of — and a solution to — your personal financial problems that will guide you through a lifetime.<br> This is the book that holds the secrets to keeping your money — and making more. </p>
 					<a href="https://www.amazon.com/Richest-Man-Babylon-George-Clason-ebook/dp/B07H7HN6DN/ref=sr_1_2?ie=UTF8&qid=1543947956&sr=8-2&keywords=the%20richest%20man%20in%20babylon&fbclid=IwAR1WmqE4etQ3obWUUTe9_6q14PXKU21RZc4dXMl34YsHM_9FWGnz_HuLT3M">The Richest Man In Babylon Amazon</a>
 					<figure class="makeover-right">
-						<p>Whether you recently got into debt due to circumstances beyond your control, or your own poor choices have kept you in debt for as long as you can remember, know this: Debt does not have to remain a way of life. Regardless of your situation, you can get on the road to financial freedom – and you can do it yourself in the next 30 days!</p>
+						<p>Whether you recently got into debt due to circumstances beyond your control,<br> or your own poor choices have kept you in debt for as long as you can remember,<br> know this: Debt does not have to remain a way of life. Regardless of your situation,<br> you can get on the road to financial freedom – and you can do it yourself in the next 30 days!</p>
 						<img src="assets/zero-debt.jpg" alt="Dave Ramseys book the total money makeover" id="zero-pic">
 					</div>
 						<figcaption>A classic book by a classic author</figcaption>
